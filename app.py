cc1ecff450a5cdd13d4a597a0fbb75434321f954
Garv Khurana <146458741+garvkhurana@users.noreply.github.com>
2025-04-10 04:43:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "Hello from Flask on EC2!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)  # Important: Use 0.0.0.0 for public access
+    app.run(host='0.0.0.0', port=5000)  
